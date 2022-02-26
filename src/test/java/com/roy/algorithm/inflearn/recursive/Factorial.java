8858ad416716613a77b7dcb06c2bf4441bf08560
roy-zz <roystreet91@gmail.com>
2022-02-26 20:02:24 +0900

    add factorial algorithm

---
+++ src/test/java/com/roy/algorithm/inflearn/recursive/Factorial.java
@@ -0,0 +1,43 @@
+package com.roy.algorithm.inflearn.recursive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// 팩토리얼
+//
+// 자연수 N이 입력되면 N!를 구하는 프로그램을 작성하세요.
+// 예를 들어 5! = 5*4*3*2*1=120입니다.
+// - 입력설명
+// 첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+// - 출력설명
+// 첫 번째 줄에 N팩토리얼 값을 출력합니다.
+// - 입력예제 1
+// 5
+// - 출력예제 1
+// 120
+@SuppressWarnings("NewClassNamingConvention")
+public class Factorial {
+
+    private int answer = 1;
+
+    public int solution1(int number) {
+        if (number == 0) {
+            return answer;
+        } else {
+            answer *= number;
+            solution1(number - 1);
+        }
+        return answer;
+    }
+
+    @Test
+    @DisplayName("팩토리얼")
+    public void main() {
+        int expectedAnswer = 120;
+        int answer = solution1(5);
+        assertEquals(expectedAnswer, answer);
+    }
+
+}
