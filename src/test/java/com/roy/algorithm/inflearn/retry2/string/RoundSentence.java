78a3add1cb726e007d15315f22f4ecd7956657bd
roy-zz <roystreet91@gmail.com>
2022-03-05 19:54:41 +0900

    retry round sentence algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/string/RoundSentence.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/string/RoundSentence.java
@@ -1,5 +1,10 @@
 package com.roy.algorithm.inflearn.retry2.string;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // 회문 문자열
 //
 // 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
@@ -15,4 +20,39 @@ package com.roy.algorithm.inflearn.retry2.string;
 // YES
 @SuppressWarnings("NewClassNamingConvention")
 public class RoundSentence {
+
+    // 직접 문자를 비교하면서 확인하는 방법
+    public String solution1(String input) {
+        input = input.toUpperCase();
+        int left = 0;
+        int right = input.length() - 1;
+        while (right >= left) {
+            if (input.charAt(left) != input.charAt(right)) {
+                return "NO";
+            }
+            left++;
+            right--;
+        }
+        return "YES";
+    }
+
+    // StringBuilder의 reverse 함수를 사용하는 방법
+    public String solution2(String input) {
+        input = input.toUpperCase();
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equals(reversed) ? "YES" : "NO";
+    }
+
+    @Test
+    @DisplayName("회문 문자열")
+    public void main() {
+        String input = "gooG";
+        String expectedAnswer = "YES";
+        String answer1 = solution1(input);
+        assertEquals(expectedAnswer, answer1);
+
+        String answer2 = solution2(input);
+        assertEquals(expectedAnswer, answer2);
+    }
+
 }
