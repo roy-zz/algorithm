486bf3ee336083f0a1f921ff2cc692b6c3b3beda
roy-zz <roystreet91@gmail.com>
2022-03-06 10:15:30 +0900

    retry fibonacci numbers algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/array/FibonacciNumbers.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/array/FibonacciNumbers.java
@@ -1,9 +1,14 @@
 package com.roy.algorithm.inflearn.retry2.array;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 피보나치 수열
 //
-// 1) 피보나키 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
+// 1) 피보나치 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
 // 2) 입력은 피보나치 수열의 총 항의 수 이다. 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.
 // - 입력설명
 // 첫 줄에 총 항수 N(3<=N<=45)이 입력된다.
@@ -15,4 +20,23 @@ package com.roy.algorithm.inflearn.retry2.array;
 // 1 1 2 3 5 8 13 21 34 55
 @SuppressWarnings("NewClassNamingConvention")
 public class FibonacciNumbers {
+
+    public int[] solution1(int size) {
+        int[] answer = new int[size];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < size; i++) {
+            answer[i] = answer[i - 2] + answer[i - 1];
+        }
+        return answer;
+    }
+
+    @Test
+    @DisplayName("피보나치 수열")
+    public void main() {
+        int[] expectedAnswer = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+        int[] answer = solution1(10);
+        assertArrayEquals(expectedAnswer, answer);
+    }
+
 }
