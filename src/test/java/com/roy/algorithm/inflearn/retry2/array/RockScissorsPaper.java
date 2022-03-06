f10800774d3ef790dce815c7aaa63afb50b5faa2
roy-zz <roystreet91@gmail.com>
2022-03-06 13:31:34 +0900

    retry rock scissors paper algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/array/RockScissorsPaper.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/array/RockScissorsPaper.java
@@ -1,5 +1,10 @@
 package com.roy.algorithm.inflearn.retry2.array;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 가위 바위 보
 //
 // A, B 두 사람이 가위바위보 게임을 합니다. 총 N번의 게임을 하여 A가 이기면 A를 출력하고, B가 이기면 B를 출력합니다. 비길 경우에는 D를 출력합니다.
@@ -9,15 +14,15 @@ package com.roy.algorithm.inflearn.retry2.array;
 // A의 정보   2 3 3 1 3
 // B의 정보   1 1 2 2 3
 // 승자       A B A B D
-// 두 사람의 각 회의 가위, 바위, 보 정보가 주어지면 각 회를 누가 이겼는지 출력하는 프로그램 을 작성하세요.
+// 두 사람의 각 회의 가위, 바위, 보 정보가 주어지면 각 회를 누가 이겼는지 출력하는 프로그램을 작성하세요.
 // - 입력설명
 // 첫 번째 줄에 게임 횟수인 자연수 N(1<=N<=100)이 주어집니다. 두 번째 줄에는 A가 낸 가위, 바위, 보 정보가 N개 주어집니다. 세 번째 줄에는 B가 낸 가위, 바위, 보 정보가 N개 주어집니다.
 // - 출력설명
 // 각 줄에 각 회의 승자를 출력합니다. 비겼을 경우는 D를 출력합니다.
 // 입력예제 1
 // 5
-// 23313
-// 11223
+// 2 3 3 1 3
+// 1 1 2 2 3
 // - 출력예제 1
 // A
 // B
@@ -26,4 +31,33 @@ package com.roy.algorithm.inflearn.retry2.array;
 // D
 @SuppressWarnings("NewClassNamingConvention")
 public class RockScissorsPaper {
+
+    public char[] solution1(int[] aUser, int[] bUser) {
+        char[] answer = new char[aUser.length];
+        for (int i = 0; i < aUser.length; i++) {
+            if (aUser[i] == bUser[i]) {
+                answer[i] = 'D';
+            } else if (aUser[i] == 1 && bUser[i] == 3) {
+                answer[i] = 'A';
+            } else if (aUser[i] == 2 && bUser[i] == 1) {
+                answer[i] = 'A';
+            } else if (aUser[i] == 3 && bUser[i] == 2) {
+                answer[i] = 'A';
+            } else {
+                answer[i] = 'B';
+            }
+        }
+        return answer;
+    }
+
+    @Test
+    @DisplayName("가위 바위 보")
+    public void main() {
+        int[] aUser = {2, 3, 3, 1, 3};
+        int[] bUser = {1, 1, 2, 2, 3};
+        char[] expectedAnswer = {'A', 'B', 'A', 'B', 'D'};
+        char[] answer = solution1(aUser, bUser);
+        assertArrayEquals(expectedAnswer, answer);
+    }
+
 }
