104295b4f045b5e5adf268702558085064b919b5
roy-zz <roystreet91@gmail.com>
2022-03-06 10:12:34 +0900

    retry calculate score algorithm

---
+++ src/test/java/com/roy/algorithm/inflearn/retry2/array/FibonacciNumbers.java
@@ -0,0 +1,18 @@
+package com.roy.algorithm.inflearn.retry2.array;
+
+
+// 피보나치 수열
+//
+// 1) 피보나키 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
+// 2) 입력은 피보나치 수열의 총 항의 수 이다. 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.
+// - 입력설명
+// 첫 줄에 총 항수 N(3<=N<=45)이 입력된다.
+// - 출력설명
+// 첫 줄에 피보나치 수열을 출력합니다.
+// - 입력예제 1
+// 10
+// - 출력예제 1
+// 1 1 2 3 5 8 13 21 34 55
+@SuppressWarnings("NewClassNamingConvention")
+public class FibonacciNumbers {
+}
