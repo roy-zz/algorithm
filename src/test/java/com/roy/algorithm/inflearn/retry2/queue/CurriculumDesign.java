7176c81c5c44783fd3453ebaa9012eaec63360a6
roy-zz <roystreet91@gmail.com>
2022-03-07 11:22:13 +0900

    retry curriculum design problems

--- src/test/java/com/roy/algorithm/inflearn/retry2/queue/CurriculumDesign.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/queue/CurriculumDesign.java
@@ -1,12 +1,20 @@
 package com.roy.algorithm.inflearn.retry2.queue;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // 교육과정 설계
 //
 // 현수는 1년 과정의 수업계획을 짜야 합니다.
 // 수업중에는 필수과목이 있습니다. 이 필수과목은 반드시 이수해야 하며, 그 순서도 정해져 있습니다.
-// 만약 총 과목이 A, B, C, D, E, F, G가 있고, 여기서 필수과목이 CBA로 주어지면 필수과목은 C, B, A과목이며 이 순서대로 꼭 수업계획을 짜야 합니다.
-// 여기서 순서란 B과목은 C과목을 이수한 후에 들어야 하고, A과목은 C와 B를 이수한 후에 들 어야 한다는 것입니다.
+// 만약 총 과목이 A, B, C, D, E, F, G가 있고, 여기서 필수과목이 CBA로 주어지면 필수과목은 C, B, A과목이며 이 순서대로 꼭 수업계획을 짜야합니다.
+// 여기서 순서란 B과목은 C과목을 이수한 후에 들어야 하고, A과목은 C와 B를 이수한 후에 들어야 한다는 것입니다.
 // 현수가 C, B, D, A, G, E로 수업계획을 짜면 제대로 된 설계이지만
 // C, G, E, A, D, B 순서로 짰다면 잘못 설계된 수업계획이 됩니다.
 // 수업계획은 그 순서대로 앞에 수업이 이수되면 다음 수업을 시작하다는 것으로 해석합니다. 수업계획서상의 각 과목은 무조건 이수된다고 가정합니다.
@@ -23,4 +31,28 @@ package com.roy.algorithm.inflearn.retry2.queue;
 // YES
 @SuppressWarnings("NewClassNamingConvention")
 public class CurriculumDesign {
+
+    public String solution1(char[] neededClasses, char[] selectedClasses) {
+        Queue<Character> queueOfNeededClasses = new LinkedList<>();
+        for (char clazz : neededClasses) {
+            queueOfNeededClasses.offer(clazz);
+        }
+        for (char clazz : selectedClasses) {
+            if (!queueOfNeededClasses.isEmpty() && queueOfNeededClasses.peek() == clazz) {
+                queueOfNeededClasses.poll();
+            }
+        }
+        return queueOfNeededClasses.isEmpty() ? "YES" : "NO";
+    }
+
+    @Test
+    @DisplayName("교육과정 설계")
+    public void main() {
+        char[] neededClasses = {'C', 'B', 'A'};
+        char[] selectedClasses = {'C', 'B', 'D', 'A', 'G', 'E'};
+        String expectedAnswer = "YES";
+        String answer = solution1(neededClasses, selectedClasses);
+        assertEquals(expectedAnswer, answer);
+    }
+
 }
