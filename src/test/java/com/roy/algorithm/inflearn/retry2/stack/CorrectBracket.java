7ff8745bd3f011f6005a982344bada29498ddad7
roy-zz <roystreet91@gmail.com>
2022-03-07 12:17:54 +0900

    retry correct bracket problems

--- src/test/java/com/roy/algorithm/inflearn/retry2/stack/CorrectBracket.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/stack/CorrectBracket.java
@@ -1,5 +1,12 @@
 package com.roy.algorithm.inflearn.retry2.stack;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // 올바른 괄호
 //
 // 괄호가 입력되면 올바른 괄호이면 “YES", 올바르지 않으면 ”NO"를 출력합니다.
@@ -8,8 +15,34 @@ package com.roy.algorithm.inflearn.retry2.stack;
 // 첫 번째 줄에 괄호 문자열이 입력됩니다. 문자열의 최대 길이는 30이다.
 // - 출력설명
 // 첫 번째 줄에 YES, NO를 출력한다.
-// - 입력예제 1 (()(()))(()
-// - 출력예제 1 NO
+// - 입력예제 1
+// (()(()))(()
+// - 출력예제 1
+// NO
 @SuppressWarnings("NewClassNamingConvention")
 public class CorrectBracket {
+
+    public String solution1(String inputs) {
+        Stack<Character> stackOfInputs = new Stack<>();
+        for (char c : inputs.toCharArray()) {
+            if (c == '(') {
+                stackOfInputs.push(c);
+            } else if (stackOfInputs.isEmpty()) {
+                return "NO";
+            } else {
+                stackOfInputs.pop();
+            }
+        }
+        return stackOfInputs.isEmpty() ? "YES" : "NO";
+    }
+
+    @Test
+    @DisplayName("올바른 괄호")
+    public void main() {
+        String inputs = "(()(()))(()";
+        String expectedAnswer = "NO";
+        String answer = solution1(inputs);
+        assertEquals(expectedAnswer, answer);
+    }
+
 }
