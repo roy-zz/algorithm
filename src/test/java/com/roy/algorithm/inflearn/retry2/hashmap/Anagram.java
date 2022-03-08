56f5e8764ddaf51025032c8d77ff0ffc44eec928
roy-zz <roystreet91@gmail.com>
2022-03-08 09:36:00 +0900

    return anagram hashmap problems

--- src/test/java/com/roy/algorithm/inflearn/retry2/hashmap/Anagram.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/hashmap/Anagram.java
@@ -1,9 +1,19 @@
 package com.roy.algorithm.inflearn.retry2.hashmap;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // 아나그램
 //
 // Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 아나그램이라고 합니다.
-// 예를 들면 AbaAeCe 와 baeeACA 는 알파벳을 나열 순서는 다르지만 그 구성을 살펴보면 A(2), a(1), b(1), C(1), e(2)로 알파벳과 그 개수가 모두 일치합니다. 즉 어느 한 단어를 재 배열하면 상대편 단어가 될 수 있는 것을 아나그램이라 합니다.
+// 예를 들면 AbaAeCe 와 baeeACA 는 알파벳을 나열 순서는 다르지만 그 구성을 살펴보면 A(2), a(1), b(1), C(1), e(2)로 알파벳과 그 개수가 모두 일치합니다.
+// 즉 어느 한 단어를 재 배열하면 상대편 단어가 될 수 있는 것을 아나그램이라 합니다.
 // 길이가 같은 두 개의 단어가 주어지면 두 단어가 아나그램인지 판별하는 프로그램을 작성하세요.
 // 아나그램 판별시 대소문자가 구분됩니다.
 // - 입력설명
@@ -22,4 +32,39 @@ package com.roy.algorithm.inflearn.retry2.hashmap;
 // NO
 @SuppressWarnings("NewClassNamingConvention")
 public class Anagram {
+
+    public String solution(String wordA, String wordB) {
+        if (wordA.length() != wordB.length()) {
+            return "NO";
+        }
+        Map<Character, Integer> mapOfWordA = new HashMap<>();
+        Map<Character, Integer> mapOfWordB = new HashMap<>();
+        for (int i = 0; i < wordA.length(); i++) {
+            mapOfWordA.put(wordA.charAt(i), mapOfWordA.getOrDefault(wordA.charAt(i), 0) + 1);
+            mapOfWordB.put(wordB.charAt(i), mapOfWordB.getOrDefault(wordB.charAt(i), 0) + 1);
+        }
+        for (char c : mapOfWordA.keySet()) {
+            if (!Objects.equals(mapOfWordA.get(c), mapOfWordB.get(c))) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+    @Test
+    @DisplayName("아나그램")
+    public void main() {
+        String wordA1 = "AbaAeCe";
+        String wordB1 = "baeeACA";
+        String expectedAnswer1 = "YES";
+        String answer1 = solution(wordA1, wordB1);
+        assertEquals(expectedAnswer1, answer1);
+
+        String wordA2 = "abaCC";
+        String wordB2 = "Caaab";
+        String expectedAnswer2 = "NO";
+        String answer2 = solution(wordA2, wordB2);
+        assertEquals(expectedAnswer2, answer2);
+    }
+
 }
