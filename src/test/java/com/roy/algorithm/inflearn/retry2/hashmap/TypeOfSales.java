5426728c18066872ee50e077db31766988b16722
roy-zz <roystreet91@gmail.com>
2022-03-08 11:07:43 +0900

    retry type of sales algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/hashmap/TypeOfSales.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/hashmap/TypeOfSales.java
@@ -1,5 +1,15 @@
 package com.roy.algorithm.inflearn.retry2.hashmap;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 매출액의 종류
 //
 // 현수의 아빠는 제과점을 운영합니다. 현수아빠는 현수에게 N일 동안의 매출기록을 주고 연속
@@ -18,10 +28,41 @@ package com.roy.algorithm.inflearn.retry2.hashmap;
 // - 출력설명
 // 첫 줄에 각 구간의 매출액 종류를 순서대로 출력합니다.
 // - 입력예제 1
-// 74
+// 7 4
 // 20 12 20 10 23 17 10
 // - 출력예제 1
 // 3 4 4 3
+// - 풀이
+// 해시맵의 값이 0일 때 비워주는 것 기억할 것!
 @SuppressWarnings("NewClassNamingConvention")
 public class TypeOfSales {
+
+    public Integer[] solution(int[] sales, int days) {
+        List<Integer> answer = new ArrayList<>();
+        Map<Integer, Integer> mapOfSales = new HashMap<>();
+        for (int i = 0; i < days - 1; i++) {
+            mapOfSales.put(sales[i], mapOfSales.getOrDefault(sales[i], 0) + 1);
+        }
+        int left = 0;
+        for (int right = days - 1; right < sales.length; right++) {
+            mapOfSales.put(sales[right], mapOfSales.getOrDefault(sales[right], 0) + 1);
+            answer.add(mapOfSales.size());
+            mapOfSales.put(sales[left],  mapOfSales.getOrDefault(sales[left], 0) - 1);
+            if (mapOfSales.get(sales[left]) == 0) {
+                mapOfSales.remove(sales[left]);
+            }
+            left++;
+        }
+        return answer.toArray(new Integer[0]);
+    }
+
+    @Test
+    @DisplayName("매출액의 종류")
+    public void main() {
+        int[] sales = {20, 12, 20, 10, 23, 17, 10};
+        Integer[] expectedAnswer = {3, 4, 4, 3};
+        Integer[] answer = solution(sales, 4);
+        assertArrayEquals(expectedAnswer, answer);
+    }
+
 }
