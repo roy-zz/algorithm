67002fb7a4b1455328c2346f2a0822d04ef3386f
roy-zz <roystreet91@gmail.com>
2022-03-09 14:29:10 +0900

    add sorting problems

---
+++ src/test/java/com/roy/algorithm/inflearn/retry2/sorting/DuplicateCheck.java
@@ -0,0 +1,30 @@
+package com.roy.algorithm.inflearn.retry2.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// 중복 확인
+//
+// 현수네 반에는 N명의 학생들이 있습니다.
+// 선생님은 반 학생들에게 1부터 10,000,000까지의 자연수 중에서 각자가 좋아하는 숫자 하나 적어내라고 했습니다.
+// 만약 N명의 학생들이 적어낸 숫자 중 중복된 숫자가 존재하면 D(duplication)를 출력하고,
+// N 명이 모두 각자 다른 숫자를 적어냈다면 U(unique)를 출력하는 프로그램을 작성하세요.
+// - 입력설명
+// 첫 번째 줄에 자연수 N(5<=N<=100,000)이 주어진다.
+// 두 번째 줄에 학생들이 적어 낸 N개의 자연수가 입력된다.
+// - 출력설명
+// 첫 번째 줄에 D 또는 U를 출력한다.
+// - 입력예제 1
+// 8
+// 20 25 52 30 39 33 43 33
+// - 출력예제 1
+// D
+@SuppressWarnings("NewClassNamingConvention")
+public class DuplicateCheck {
+
+    @Test
+    @DisplayName("중복 확인")
+    public void main() {
+    }
+
+}
---
+++ src/test/java/com/roy/algorithm/inflearn/retry2/sorting/SortingCoordinate.java
@@ -0,0 +1,35 @@
+package com.roy.algorithm.inflearn.retry2.sorting;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// 좌표 정렬
+//
+// N개의 평면상의 좌표(x, y)가 주어지면 모든 좌표를 오름차순으로 정렬하는 프로그램을 작성하세요.
+// 정렬기준은 먼저 x값의 의해서 정렬하고, x값이 같을 경우 y값에 의해 정렬합니다.
+// - 입력설명
+// 첫째 줄에 좌표의 개수인 N(3<=N<=100,000)이 주어집니다.
+// 두 번째 줄부터 N개의 좌표가 x, y 순으로 주어집니다. x, y값은 양수만 입력됩니다.
+// - 출력설명
+// N개의 좌표를 정렬하여 출력하세요.
+// - 입력예제 1 5
+// 27
+// 13
+// 12
+// 25
+// 36
+// - 출력예제 1
+// 12
+// 13
+// 25
+// 27
+// 36
+@SuppressWarnings("NewClassNamingConvention")
+public class SortingCoordinate {
+
+    @Test
+    @DisplayName("좌표 정렬")
+    public void main() {
+    }
+
+}
