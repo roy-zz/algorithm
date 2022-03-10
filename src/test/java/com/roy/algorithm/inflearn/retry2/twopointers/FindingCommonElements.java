9b8228541fed04950b59ba36dfd11980285bb9ba
roy-zz <roystreet91@gmail.com>
2022-03-10 15:49:48 +0900

    retry finding common elements algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/twopointers/FindingCommonElements.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/twopointers/FindingCommonElements.java
@@ -3,6 +3,12 @@ package com.roy.algorithm.inflearn.retry2.twopointers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 공통 원소 구하기
 //
 // A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
@@ -22,14 +28,33 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NewClassNamingConvention")
 public class FindingCommonElements {
 
+    public Integer[] solution1(int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int arr1Position = 0;
+        int arr2Position = 0;
+        while (arr1Position < arr1.length && arr2Position < arr2.length) {
+            if (arr1[arr1Position] < arr2[arr2Position]) {
+                arr1Position++;
+            } else if (arr1[arr1Position] > arr2[arr2Position]) {
+                arr2Position++;
+            } else {
+                answer.add(arr1[arr1Position++]);
+                arr2Position++;
+            }
+        }
+        return answer.toArray(new Integer[0]);
+    }
+
     @Test
     @DisplayName("공통 원소 구하기")
     public void main() {
         int[] arr1 = {1, 3, 9, 5, 2};
         int[] arr2 = {3, 2, 5, 7, 8};
         Integer[] expectedAnswer = {2, 3, 5};
-//        Integer[] answer1 = solution1(arr1, arr2);
-//        assertArrayEquals(expectedAnswer, answer1);
+        Integer[] answer1 = solution1(arr1, arr2);
+        assertArrayEquals(expectedAnswer, answer1);
     }
 
 }
