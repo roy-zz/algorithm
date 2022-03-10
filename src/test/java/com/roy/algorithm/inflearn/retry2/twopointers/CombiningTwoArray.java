193140179dbc947af123072519cb4ae1ef3f13c2
roy-zz <roystreet91@gmail.com>
2022-03-10 15:32:47 +0900

    retry combining two array algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/twopointers/CombiningTwoArray.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/twopointers/CombiningTwoArray.java
@@ -3,6 +3,14 @@ package com.roy.algorithm.inflearn.retry2.twopointers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 두 배열 합치기
 //
 // 오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
@@ -22,14 +30,45 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NewClassNamingConvention")
 public class CombiningTwoArray {
 
+    public Integer[] solution1(int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(Arrays.stream(arr1).boxed().collect(Collectors.toList()));
+        answer.addAll(Arrays.stream(arr2).boxed().collect(Collectors.toList()));
+        Collections.sort(answer);
+        return answer.toArray(new Integer[0]);
+    }
+
+    public Integer[] solution2(int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        int arr1Position = 0;
+        int arr2Position = 0;
+        while (arr1Position < arr1.length && arr2Position < arr2.length) {
+            if (arr1[arr1Position] <= arr2[arr2Position]) {
+                answer.add(arr1[arr1Position++]);
+            } else {
+                answer.add(arr2[arr2Position++]);
+            }
+        }
+        while (arr1Position < arr1.length) {
+            answer.add(arr1[arr1Position++]);
+        }
+        while (arr2Position < arr2.length) {
+            answer.add(arr2[arr2Position++]);
+        }
+        return answer.toArray(new Integer[0]);
+    }
+
     @Test
     @DisplayName("두 배열 합치기")
     public void main() {
         int[] arr1 = {1, 3, 5};
         int[] arr2 = {2, 3, 6, 7, 9};
         Integer[] expectedAnswer = {1, 2, 3, 3, 5, 6, 7, 9};
-//        Integer[] answer1 = solution1(arr1, arr2);
-//        assertArrayEquals(expectedAnswer, answer1);
+        Integer[] answer1 = solution1(arr1, arr2);
+        assertArrayEquals(expectedAnswer, answer1);
+
+        Integer[] answer2 = solution2(arr1, arr2);
+        assertArrayEquals(expectedAnswer, answer2);
     }
 
 }
