36ab30d938c311582e263951d820457ecd75c702
roy-zz <roystreet91@gmail.com>
2022-03-11 11:54:43 +0900

    retry recursive function algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/recursive/RecursiveFunction.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/recursive/RecursiveFunction.java
@@ -3,6 +3,11 @@ package com.roy.algorithm.inflearn.retry2.recursive;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 재귀함수
 //
 // 자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요.
@@ -17,9 +22,25 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NewClassNamingConvention")
 public class RecursiveFunction {
 
+    private final List<Integer> answer = new ArrayList<>();
+
+    public Integer[] solution(int input) {
+        if (input == 0) {
+            return answer.toArray(new Integer[0]);
+        } else {
+            solution(input - 1);
+            answer.add(input);
+        }
+        return answer.toArray(new Integer[0]);
+    }
+
     @Test
     @DisplayName("재귀함수")
     public void main() {
+        int input = 3;
+        Integer[] expectedAnswer = {1, 2, 3};
+        Integer[] answer = solution(input);
+        assertArrayEquals(expectedAnswer, answer);
     }
 
 }
