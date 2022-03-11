2870d1a413362efadc435f8bcf18f9032e4258c6
roy-zz <roystreet91@gmail.com>
2022-03-11 13:22:17 +0900

    retry binary tree search bfs algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/dfsbfs/BinaryTreeSearchBFS.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/dfsbfs/BinaryTreeSearchBFS.java
@@ -3,6 +3,10 @@ package com.roy.algorithm.inflearn.retry2.dfsbfs;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 이진트리 순회(넓이우선탐색: 레벨탐색, BFS)
 //
 // 아래와 같은 이진트리를 레벨탐색 연습해보세요.
@@ -14,19 +18,45 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NewClassNamingConvention")
 public class BinaryTreeSearchBFS {
 
+    static class Node {
+        Node leftSon;
+        Node rightSon;
+        int data;
+        public Node (int data) {
+            this.data = data;
+        }
+    }
+
+    private final List<Integer> answer = new ArrayList<>();
+
+    public void solution(Node root) {
+        Queue<Node> queueOfNode = new LinkedList<>();
+        queueOfNode.offer(root);
+        while (!queueOfNode.isEmpty()) {
+            Node tempNode = queueOfNode.poll();
+            answer.add(tempNode.data);
+            if (Objects.nonNull(tempNode.leftSon)) {
+                queueOfNode.offer(tempNode.leftSon);
+            }
+            if (Objects.nonNull(tempNode.rightSon)) {
+                queueOfNode.offer(tempNode.rightSon);
+            }
+        }
+    }
+
     @Test
     @DisplayName("이진트리 순회(넓이우선탐색: 레벨탐색)")
     public void main() {
-//        Node root = new Node(1);
-//        root.leftSon = new Node(2);
-//        root.rightSon = new Node(3);
-//        root.leftSon.leftSon = new Node(4);
-//        root.leftSon.rightSon = new Node(5);
-//        root.rightSon.leftSon = new Node(6);
-//        root.rightSon.rightSon = new Node(7);
-//        Integer[] expectedAnswer1 = {1, 2, 3, 4, 5, 6, 7};
-//        solution1(root);
-//        assertArrayEquals(expectedAnswer1, answer.toArray());
+        Node root = new Node(1);
+        root.leftSon = new Node(2);
+        root.rightSon = new Node(3);
+        root.leftSon.leftSon = new Node(4);
+        root.leftSon.rightSon = new Node(5);
+        root.rightSon.leftSon = new Node(6);
+        root.rightSon.rightSon = new Node(7);
+        Integer[] expectedAnswer1 = {1, 2, 3, 4, 5, 6, 7};
+        solution(root);
+        assertArrayEquals(expectedAnswer1, answer.toArray());
     }
 
 }
