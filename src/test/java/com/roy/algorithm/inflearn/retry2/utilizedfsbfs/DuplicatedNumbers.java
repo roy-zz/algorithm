e20bac7996569274bed8b166b3320448e253887c
roy-zz <roystreet91@gmail.com>
2022-03-12 10:53:46 +0900

    retry duplicated numbers algorithm

--- src/test/java/com/roy/algorithm/inflearn/retry2/utilizedfsbfs/DuplicatedNumbers.java
+++ src/test/java/com/roy/algorithm/inflearn/retry2/utilizedfsbfs/DuplicatedNumbers.java
@@ -3,6 +3,12 @@ package com.roy.algorithm.inflearn.retry2.utilizedfsbfs;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 // 중복가능한 순열 구하기
 //
 // 1부터 N까지 번호가 적힌 구슬이 있습니다.
@@ -27,9 +33,36 @@ import org.junit.jupiter.api.Test;
 @SuppressWarnings("NewClassNamingConvention")
 public class DuplicatedNumbers {
 
+    private static final int TOTAL_NUMBER = 3;
+    private static final int TOTAL_COUNT = 2;
+    List<int[]> answer = new ArrayList<>();
+
+    public void solution(int[] tempNumbers, int level) {
+        if (level == TOTAL_COUNT) {
+            answer.add(tempNumbers.clone());
+        } else {
+            for (int i = 1; i <= TOTAL_NUMBER; i++) {
+                if (Objects.isNull(tempNumbers)) {
+                    tempNumbers = new int[TOTAL_COUNT];
+                }
+                tempNumbers[level] = i;
+                solution(tempNumbers, level + 1);
+            }
+        }
+    }
+
     @Test
     @DisplayName("중복가능한 순열 구하기")
     public void main() {
+        int[][] expectedAnswer = {
+                {1, 1}, {1, 2}, {1, 3},
+                {2, 1}, {2, 2}, {2, 3},
+                {3, 1}, {3, 2}, {3, 3}
+        };
+        solution(null, 0);
+        for (int i = 0; i < answer.size(); i++) {
+            assertArrayEquals(answer.get(i), expectedAnswer[i]);
+        }
     }
 
 }
