d27e25d1fe5aadcadac6f9aab178936945caaf1c
roy-zz <roystreet91@gmail.com>
2022-03-13 17:46:29 +0900

    add graph problems

---
+++ src/test/java/com/roy/algorithm/inflearn/retry3/graph/ShortestPath.java
@@ -0,0 +1,44 @@
+package com.roy.algorithm.inflearn.retry3.graph;
+
+// 그래프 최단거리(BFS)
+//
+// 다음 그래프에서 1번 정점에서 각 정점으로 가는 최소 이동 간선수를 출력하세요.
+// - 입력설명
+// 첫째 줄에는 정점의 수 N(1<=N<=20)와 간선의 수 M가 주어진다. 그 다음부터 M줄에 걸쳐 연결정보가 주어진다.
+// - 출력설명
+// 1번 정점에서 각 정점으로 가는 최소 간선수를 2번 정점부터 차례대로 출력하세요.
+// - 입력예제 1
+// 6 9
+// 1 3
+// 1 4
+// 2 1
+// 2 5
+// 3 4
+// 4 5
+// 4 6
+// 6 2
+// 6 5
+// - 출력예제 1
+// 2 3
+// 3 1
+// 4 1
+// 5 2
+// 6 2
+// HARD
+@SuppressWarnings("NewClassNamingConvention")
+public class ShortestPath {
+
+//    @Test
+//    @DisplayName("그래프 최단거리(BFS)")
+//    public void main() {
+//        for (int i = 0; i <= VERTEXES; i++) {
+//            graph.add(new ArrayList<>());
+//        }
+//        for (int[] connection : CONNECTIONS) {
+//            graph.get(connection[0]).add(connection[1]);
+//        }
+//        solution(1);
+//        assertArrayEquals(EXPECTED_DISTANCES, DISTANCES);
+//    }
+
+}
